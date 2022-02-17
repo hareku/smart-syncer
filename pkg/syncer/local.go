cc10b4f848ee5156df316c69a3a4d3975fb1c27e
hareku <hareku908@gmail.com>
2022-02-17 18:20:41 +0900

    fixup! feat: implement app

--- pkg/syncer/local.go
+++ pkg/syncer/local.go
@@ -28,13 +28,13 @@ type localStorage struct {
 func (s *localStorage) List(ctx context.Context, path string, depth int) ([]LocalObject, error) {
 	s.res = []LocalObject{}
 
-	if err := s.recursive(ctx, path, depth, 1); err != nil {
-		return nil, fmt.Errorf("recursive walk failed: %w", err)
+	if err := s.recurse(ctx, path, depth, 1); err != nil {
+		return nil, fmt.Errorf("recursive walking failed: %w", err)
 	}
 	return s.res, nil
 }
 
-func (s *localStorage) recursive(ctx context.Context, path string, depth int, currentDepth int) error {
+func (s *localStorage) recurse(ctx context.Context, path string, depth int, currentDepth int) error {
 	entries, err := os.ReadDir(path)
 	if err != nil {
 		return fmt.Errorf("failed to read %q as a directory: %w", path, err)
@@ -42,7 +42,7 @@ func (s *localStorage) recursive(ctx context.Context, path string, depth int, cu
 	for _, e := range entries {
 		if e.IsDir() && currentDepth < depth {
 			nextPath := filepath.Join(path, e.Name())
-			if err := s.recursive(ctx, nextPath, depth, currentDepth+1); err != nil {
+			if err := s.recurse(ctx, nextPath, depth, currentDepth+1); err != nil {
 				return fmt.Errorf("error in path %q depth %d: %w", nextPath, currentDepth+1, err)
 			}
 			continue
